f6f1e6b644d32b95b2905491cceaa0ebb0b9fa6f
Yiyeon Kim <yiyeonkim@yiyeons-mbp.attlocal.net>
2023-12-16 09:22:31 -0800

    configured firebase

--- chatroom/app.config.js
+++ chatroom/app.config.js
@@ -28,7 +28,7 @@ export default {
     "web": {
       "favicon": "./assets/favicon.png"
     },
-    extra: {
+    "extra": {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
