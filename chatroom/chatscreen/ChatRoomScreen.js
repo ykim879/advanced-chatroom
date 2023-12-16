b98c3e53c8c41fdbd5af209159548ca63a4facd4
Yiyeon Kim <yiyeonkim@yiyeons-mbp.attlocal.net>
2023-12-16 10:00:43 -0800

    enabled chat functionality

--- chatroom/chatscreen/ChatRoomScreen.js
+++ chatroom/chatscreen/ChatRoomScreen.js
@@ -1,44 +1,71 @@
 // ChatRoomScreen.js
-import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, TextInput, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { onSnapshot, query, collection, orderBy, addDoc } from "firebase/firestore";
+import React, { useState, useRef, useLayoutEffect, useCallback } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { database } from "../config/firebase";
 
 const ChatRoomScreen = ({ route }) => {
   const { chatName } = route.params;
 
   const [inputKey, setInputKey] = useState(1); // Add key state
   const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
 
   const flatListRef = useRef();
 
-  useEffect(() => {
-    // Simulating initial messages
-    setMessages([
-      { id: '1', text: 'Hello!', sender: 'User 1' },
-      { id: '2', text: 'Hi there!', sender: 'User 2' },
-    ]);
+  useLayoutEffect(() => {
+    const collectionRef = collection(database, "mockChats");
+    const q = query(collectionRef, orderBy("createdAt", "desc"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setMessages(
+        snapshot.docs.map((doc) => ({
+          _id: doc.id,
+          createdAt: doc.data().createdAt,
+          text: doc.data().text,
+          sender: doc.data().sender,
+        }))
+      );
+    });
+    return () => unsubscribe();
   }, []);
 
   const sendMessage = () => {
-    if (inputText.trim() !== '') {
-      const newMessage = { id: `${messages.length + 1}`, text: inputText, sender: 'User 1' };
+    if (inputText.trim() !== "") {
+      const newMessage = {
+        _id: `${messages.length + 1}`,
+        text: inputText,
+        sender: "User 1",
+        createdAt: new Date().toUTCString(),
+      };
       setMessages([...messages, newMessage]);
-      setInputText('');
+      addDoc(collection(database, "mockChats"), {...newMessage});
+      setInputText("");
       setInputKey((prevKey) => prevKey + 1);
-      // Scroll to the bottom when a new message is sent
       flatListRef.current.scrollToEnd({ animated: true });
     }
-  };
+  }
 
-  const handleKeyPress = (e) => {
-    if (e.nativeEvent.key === 'Enter' && !e.nativeEvent.shiftKey) {
-      // Prevent default behavior (new line) and send the message
+  // handle to submit message when user pressed enter without shiftKey.
+  const handleKeyPress = useCallback((e) => {
+    if (e.nativeEvent.key === "Enter" && !e.nativeEvent.shiftKey) {
       sendMessage();
-    } 
-  };
+    }
+  });
 
   const renderItem = ({ item }) => (
-    <View style={item.sender === 'User 1' ? styles.sentMessage : styles.receivedMessage}>
+    <View
+      style={
+        item.sender === "User 1" ? styles.sentMessage : styles.receivedMessage
+      }
+    >
       <Text style={styles.messageText}>{item.text}</Text>
     </View>
   );
@@ -52,6 +79,7 @@ const ChatRoomScreen = ({ route }) => {
         keyExtractor={(item) => item.id}
         contentContainerStyle={styles.messagesContainer}
         showsVerticalScrollIndicator={false}
+        inverted
       />
       <View style={styles.inputContainer}>
         <TextInput
@@ -74,45 +102,45 @@ const ChatRoomScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
   },
   messagesContainer: {
     padding: 16,
   },
   sentMessage: {
-    alignSelf: 'flex-end',
-    backgroundColor: '#E4EFE7',
-    borderColor: '#064420',
+    alignSelf: "flex-end",
+    backgroundColor: "#E4EFE7",
+    borderColor: "#064420",
     borderWidth: 1,
     borderRadius: 8,
     padding: 8,
     marginVertical: 4,
-    maxWidth: '70%',
+    maxWidth: "70%",
   },
   receivedMessage: {
-    alignSelf: 'flex-start',
-    backgroundColor: '#E4EFE7',
-    borderColor: '#064420',
+    alignSelf: "flex-start",
+    backgroundColor: "#E4EFE7",
+    borderColor: "#064420",
     borderWidth: 1,
     borderRadius: 8,
     padding: 8,
     marginVertical: 4,
-    maxWidth: '70%',
+    maxWidth: "70%",
   },
   messageText: {
-    color: 'black',
+    color: "black",
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingHorizontal: 16,
     paddingVertical: 8,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   input: {
     flex: 1,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 10,
     paddingHorizontal: 16,
     paddingVertical: 8,
@@ -120,16 +148,16 @@ const styles = StyleSheet.create({
     maxHeight: 100,
   },
   sendButton: {
-    backgroundColor: '#064420',
+    backgroundColor: "#064420",
     borderRadius: 10,
     paddingVertical: 8,
     paddingHorizontal: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   sendButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
 
