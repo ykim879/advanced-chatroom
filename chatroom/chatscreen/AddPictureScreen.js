a20ea1b8e7d21e1c6514a8459735f313411f4c96
Yiyeon Kim <yiyeonkim@yiyeons-mbp.attlocal.net>
2023-12-20 17:04:02 -0800

    added uploading image functionality

--- chatroom/chatscreen/AddPictureScreen.js
+++ chatroom/chatscreen/AddPictureScreen.js
@@ -1,15 +1,69 @@
 // CameraModal.js
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { ImagePicker } from 'expo-image-picker';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { googleCloudVisionApiKey } from '../apiKeys';
 
 const AddPictureScreen = ({ onClose }) => {
 
+  const [imageUri, setImageUri] = useState(null);
+
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync(
+        {
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        }
+      );
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (err) {
+      //Todo
+      console.log("error", err);
+    }
+  }
+
+  const analyzeImage = async () => {
+    try {
+      if (!imageUri) {
+        //Todo
+        alert("Select Image First");
+        return;
+      }
+      const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${googleCloudVisionApiKey}`;
+      const imageData = await FileSystem.readAsStringAsync(imageUri, {
+        encoding: FileSystem.EncodingType.Base64,
+      })
+
+      const requestData = {
+        requests: [
+          {
+            image: {
+              content: imageData,
+            },
+            features: [{type: 'LABEL_DETECTION', maxResult: 5}],
+          }
+        ]
+      }
+
+      const apiResponse = await axios.post(apiUrl, requestData);
+      console.log(apiResponse.data.responses[0].labelAnnotations);
+    } catch (err) {
+      //Todo
+    }
+  }
   return (
     <View style={styles.modalContent}>
-      <TouchableOpacity style={styles.captureButton}>
+      <TouchableOpacity style={styles.captureButton} onPress={pickImage}>
         <Text style={styles.captureButtonText}>Upload Picture</Text>
       </TouchableOpacity>
+      {imageUri && (
+        <Image source = {{uri: imageUri}} style= {{width: 200, height: 200}}/>
+      )}
       <TouchableOpacity style={styles.closeButton} onPress={onClose}>
         <Text style={styles.closeButtonText}>Close</Text>
       </TouchableOpacity>
